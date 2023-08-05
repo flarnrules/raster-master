2d2722092a1425891f74ed7a2430a213f92e1c56
flarnrules <breifenberg11@gmail.com>
2023-08-05 02:11:43 -0500

    create textual subdirectory

--- setup_env.sh
+++ setup_env.sh
@@ -2,3 +2,5 @@
 
 alias activate_image_to_ascii='source ~/repos/raster-master/image_to_ascii/image_to_ascii_env/bin/activate'
 alias activate_image_collage='source ~/repos/raster-master/image_collage/image_collage_env/bin/activate'
+alias activate_textual='source ~/repos/raster-master/textual/textual_env/bin/activate'
+
